98424572ac78dc69ee55615b98b3966c750f852e
DavidFarewell <david.castillo.architect@gmail.com>
2025-02-24 20:30:50 +0000

    Contador React Ganas de Morir - DavidFarewell

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-//Bootstrap
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
+import "../styles/index.css";
+import SecondsCounter from "./components/SecondsCounter";
 
-// index.css'
-import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+let counter = 0; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+const renderCounter = () => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <SecondsCounter seconds={counter} />
+  );
+  counter++;
+};
+
+
+setInterval(renderCounter, 1000);
